10f44fb5ff39170886c6f29a5138f0d9c751ff1b
peter@tsiampas.com <peter@tsiampas.com>
2024-04-04 17:06:16 +1100

    more stuff

--- system_install.sh
+++ system_install.sh
@@ -3,7 +3,7 @@
 echo "Starting system-wide installation tasks..."
 # System update and package installation
 pacman -Syu --noconfirm
-pacman -Sy --needed --noconfirm base-devel git neovim btop python3 unzip openssh flameshot nodejs npm kitty ttf-font-awesome
+pacman -Sy --needed --noconfirm base-devel git neovim btop python3 unzip openssh flameshot nodejs npm kitty ttf-font-awesome zsh
 systemctl enable sshd
 systemctl start sshd
 echo "System-wide installations complete."
--- user_install.sh
+++ user_install.sh
@@ -14,5 +14,29 @@ else
     cd .. && rm -rf yay
 
 fi
+EDITOR=true yay -S brave-bin autojump --noconfirm --needed
+
+# Install all my fonts
+mkdir -p ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d ~/.fonts
+# Reload Font
+fc-cache -vf
+# Remove zip files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/ptsiampas/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
 echo "User-specific installations complete."
 
