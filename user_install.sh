377a2e962e8d740ee2d0347ad169e4f7676a9fbb
peter@tsiampas.com <peter@tsiampas.com>
2024-04-04 18:44:56 +1100

    Re-Write of user Install

--- user_install.sh
+++ user_install.sh
@@ -2,40 +2,47 @@
 
 echo "Starting user-specific installation tasks..."
 
-#### Check for yay ####
-ISYAY=/sbin/yay
-if [ -f "$ISYAY" ]; then 
-    echo -e "yay was located, moving on."
-else 
-    echo -e "Yay was NOT located installing"
+# Check and install yay if not present
+if ! command -v yay &> /dev/null; then
+    echo "yay was NOT located, installing..."
     git clone https://aur.archlinux.org/yay.git
-    cd yay
+    cd yay || exit
     makepkg -si --noconfirm
     cd .. && rm -rf yay
-
+else
+    echo "yay was located, moving on."
 fi
+
+# Install packages with yay
 EDITOR=true yay -S brave-bin autojump --noconfirm --needed
 
-# Install all my fonts
+# Install fonts
 mkdir -p ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d ~/.fonts
-# Reload Font
+for font in FiraCode Meslo; do
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/${font}.zip"
+    unzip "${font}.zip" -d ~/.fonts
+    rm "${font}.zip"
+done
 fc-cache -vf
-# Remove zip files
-rm ./FiraCode.zip ./Meslo.zip
 
 # Install Nordzy cursor
 git clone https://github.com/alvatip/Nordzy-cursors
-cd Nordzy-cursors
+cd Nordzy-cursors || exit
 ./install.sh
-cd $builddir
-rm -rf Nordzy-cursors
+cd .. && rm -rf Nordzy-cursors
 
+# Install Oh My Zsh and Powerlevel10k
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# Backup and replace .zshrc if it exists
+if [ -f "$HOME/.zshrc" ]; then
+    mv "$HOME/.zshrc" "$HOME/.zshrc.old"
+fi
+ln -s ./dotfiles/zshrc "$HOME/.zshrc"
+ln -s ./dotfiles/p10k.zsh "$HOME/.p10k.zsh"
+
+# Setup Neovim
 git clone https://github.com/ptsiampas/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 echo "User-specific installations complete."
