7f0fb07b9c482a212af802f1239869cdcf585110
peter@tsiampas.com <peter@tsiampas.com>
2024-04-06 09:11:50 +1100

    easier to read packages

--- scripts/system_install.sh
+++ scripts/system_install.sh
@@ -3,10 +3,12 @@
 echo "Starting system-wide installation tasks..."
 # System update and package installation
 pacman -Syu --noconfirm
-pacman -Sy --needed --noconfirm base-devel git neovim btop python3 unzip openssh flameshot nodejs npm kitty ttf-font-awesome zsh
-pacman -Sy --needed --noconfirm \
+pacman -Sy --needed --noconfirm base-devel git neovim btop python3 \
+    unzip openssh flameshot nodejs npm \
+    kitty ttf-font-awesome zsh \
     obsidian \
-    stow
+    stow \
+
 systemctl enable sshd
 systemctl start sshd
 echo "System-wide installations complete."
