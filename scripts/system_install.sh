e46bce515df7595b92d8989ce176c85d16164651
peter@tsiampas.com <peter@tsiampas.com>
2024-04-05 18:50:18 +1100

    Added stow to the system package list

--- scripts/system_install.sh
+++ scripts/system_install.sh
@@ -5,7 +5,8 @@ echo "Starting system-wide installation tasks..."
 pacman -Syu --noconfirm
 pacman -Sy --needed --noconfirm base-devel git neovim btop python3 unzip openssh flameshot nodejs npm kitty ttf-font-awesome zsh
 pacman -Sy --needed --noconfirm \
-    obsidian
+    obsidian \
+    stow
 systemctl enable sshd
 systemctl start sshd
 echo "System-wide installations complete."
